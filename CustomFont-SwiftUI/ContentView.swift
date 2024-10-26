3106fdd7ede022015b41dcea7d2bd75da12c2688
mulgir-govind <govind.mulgir.17@gmail.com>
2024-10-26 16:29:03 +0530

    Initial Commit

---
+++ CustomFont-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomFont-SwiftUI
+//
+//  Created by Govind-Mulgir on 26/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomFont-SwiftUI/CustomFont_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFont_SwiftUIApp.swift
+//  CustomFont-SwiftUI
+//
+//  Created by Govind-Mulgir on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomFont_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
